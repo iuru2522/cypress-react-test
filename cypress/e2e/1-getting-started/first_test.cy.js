20f2550abbb79d213a726c2b972a7b79ba1b5b86
JULIA <ternoboy@gmail.com>
2022-08-06 00:03:11 -0400

    test commit from Windows machine

--- cypress/e2e/1-getting-started/first_test.cy.js
+++ cypress/e2e/1-getting-started/first_test.cy.js
@@ -1,12 +1,11 @@
-describe('My first test', () => { 
-    it('Does not do much', () => {
-     expect(true).to.equl(true)   
-    })
- })
+describe('My first test', () => {
+  it('Does not do much', () => {
+    expect(true).to.equl(true);
+  });
+});
 
- describe('My First Test', () => {
-    it('Does not do much!', () => {
-      expect(true).to.equal(false)
-    })
-  })
-  
+//  describe('My First Test', () => {
+//     it('Does not do much!', () => {
+//       expect(true).to.equal(false)
+//     })
+//   })
