2a7843c73bd5fabbcab10407bf7ae66f4fa9e1ae
JULIA <ternoboy@gmail.com>
2022-08-06 00:55:30 -0400

    aliasing test added

---
+++ cypress/e2e/1-getting-started/aliasing.cy.js
@@ -0,0 +1,35 @@
+//how to test 2 input instead of 1 input on the page
+
+//using specific selector
+//[data-cy="input-last-name"]
+//[data-cy="input-last-name"]
+
+describe('Text box with max characters', () => {
+  it('displays the appropriate remaining characters count', () => {
+    cy.visit('http://localhost:3000/example-3');
+
+    cy.get('[data-cy="last-name-chars-left-count"]').as('charsLeftSpan');
+
+    cy.get('@charsLeftSpan').invoke('text').should('equal', '15');
+
+    cy.get('[data-cy="input-last-name"]').as('charInput');
+
+    cy.get('@charInput').type('hello');
+
+    cy.get('@charsLeftSpan').invoke('text').should('equal', '10');
+
+    cy.get('@charInput').type(' my friend');
+
+    cy.get('@charsLeftSpan').invoke('text').should('equal', '0');
+  });
+
+  it('prevents the user from typing more character once max is exceeded', () => {
+    cy.visit('http:localhost:3000/example-3');
+
+    cy.get('[data-cy="input-last-name"]').as('charInput');
+
+    cy.get('@charInput').type('asdfghjklqwertyuio');
+
+    cy.get('@charInput').should('have.attr', 'value', 'asdfghjklqwerty');
+  });
+});
