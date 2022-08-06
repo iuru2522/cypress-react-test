d60bfd9273a115bf81a1d1fb6294d91e626bd66e
Iurii Manastyrskyi <ternoboy@gmail.com>
2022-08-05 23:56:26 -0400

    test added

---
+++ cypress/e2e/1-getting-started/first_test.cy.js
@@ -0,0 +1,12 @@
+describe('My first test', () => { 
+    it('Does not do much', () => {
+     expect(true).to.equl(true)   
+    })
+ })
+
+ describe('My First Test', () => {
+    it('Does not do much!', () => {
+      expect(true).to.equal(false)
+    })
+  })
+  
